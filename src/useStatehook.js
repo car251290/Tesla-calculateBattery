8e14463200add6bb19822f226dbe9045be1c0099
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2023-04-19 18:05:52 -0700

    Add files via upload

---
+++ src/useStatehook.js
@@ -0,0 +1,16 @@
+
+import { useState } from 'react';
+
+
+const useStatesHooks = () => {
+    const [carstats,setCarstats] = useState([]);
+    const [config,setConfig] = useState({
+      speed: 55,
+      temperature: 20,
+      climate: true,
+      wheels: 19
+     });
+     return { carstats,config };
+  }
+  
+export default useStatesHooks;

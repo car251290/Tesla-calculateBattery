fcea9e5bfd676459a8a1c9704e85a12e3db292b1
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2023-04-19 18:06:34 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-function App() {
+import TeslaBattery from './Component/cotainers/TeslaBattery';
+import Header from './Component/Header/Header';
+
+const App= () => {
+  const counterDefaultVal = {
+    speed: {
+      title: "Speed",
+      unit: "mph",
+      step: 5,
+      min: 45,
+      max: 70
+    },
+    temperature: {
+      title: "Outside Temperature",
+      unit: "°",
+      step: 10,
+      min: -10,
+      max: 40
+    }
+  };
+
   return (
     <div className="App">
-      <Header />    
+      <Header />  
+      <TeslaBattery counterDefaultVal={counterDefaultVal} />
     </div>
   );
 }
